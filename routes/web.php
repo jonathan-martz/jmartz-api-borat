e57264982794645e417b3c6aaedff5b80008b5fc
jonathan <bitbucket@jmartz.de>
2019-10-25 20:57:24 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,10 @@
 	$router->get('/', function() use ($router){
 		return $router->app->version();
 	});
-	$router->post('/login/user', 'LoginController@user');
-    $router->post('/register/user', 'RegisterController@user');
 
-    $router->get('/navigation', 'NavigationController@navigation');
+    $router->get('/packages.json', 'BoratController@packages');
+    $router->get('/p/{vendor}/{module}.json', 'BoratController@package');
 
 	$router->group(['middleware' => 'auth'], function() use ($router){
-		$router->post('/user/todo/select', 'TodoController@select');
-		$router->post('/user/todo/view', 'TodoController@view');
-		$router->post('/user/todo/add', 'TodoController@add');
-		$router->post('/user/todo/edit', 'TodoController@edit');
-		$router->post('/user/todo/delete', 'TodoController@delete');
-		$router->post('/user/todo/autocomplete', 'TodoController@autocomplete');
 
-        $router->post('/user/view', 'UserController@view');
 	});
